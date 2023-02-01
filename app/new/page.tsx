7783c584857047d754ccf0efdc5c079ac726be3e
eulambp <eula001@proton.me>
2023-02-01 19:03:40 +0000

    getting linkedin and company summaries

---
+++ app/new/page.tsx
@@ -0,0 +1,160 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Cheerio } from 'cheerio';
+
+import { ArrowSmRightIcon } from '@heroicons/react/outline';
+
+export default function Page() {
+  const [inputs, setInputs] = useState({
+    company: '',
+    linkedin: '',
+    email: '',
+  });
+  const [linkedinData, setLinkedinData] = useState({
+    name: '',
+    title: '',
+    education: '',
+    job: '',
+  });
+  const [companyData, setCompanyData] = useState('');
+  let [preview, setPreview] = useState('');
+
+  const handleChange = (e: any) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log('form submitted with these inputs', inputs);
+
+    const linkedinData = await fetch('/api/getLinkedin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs.linkedin),
+    });
+
+    const linkedin = await linkedinData.json();
+    setLinkedinData(linkedin);
+
+    const companyData = await fetch('/api/getCompany', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs.company),
+    });
+
+    const company = await companyData.json();
+    setCompanyData(company.body);
+
+    const data = {
+      linkedin,
+      company,
+    };
+
+    console.log('data', data);
+  };
+
+  return (
+    <main>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <form onSubmit={handleSubmit}>
+          <div className="max-w-3xl mx-auto">
+            <div className="py-7">
+              <label
+                htmlFor="company"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Company website (*)
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  required
+                  name="company"
+                  id="company"
+                  value={inputs.company}
+                  onChange={handleChange}
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="https://www.example.com"
+                />
+              </div>
+            </div>
+            <div className="pb-7">
+              <label
+                htmlFor="linkedin"
+                className="block text-sm font-medium text-gray-700"
+              >
+                LinkedIn profile (*)
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  required
+                  name="linkedin"
+                  id="linkedin"
+                  value={inputs.linkedin}
+                  onChange={handleChange}
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="https://www.linkedin.com/in/seaneula/"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={inputs.email}
+                  onChange={handleChange}
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="you@example.com"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="inline-flex items-center px-4 py-2 mt-5 border border-transparent text-sm rounded-md bg-indigo-300 hover:bg-indigo-400"
+            >
+              Next
+              <ArrowSmRightIcon className="h-5 w-5 text-gray-800 ml-1" />
+            </button>
+          </div>
+        </form>
+        <div className="mt-8">
+          <label
+            htmlFor="comment"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Preview of email
+          </label>
+          <div className="mt-1">
+            <textarea
+              rows={4}
+              name="comment"
+              id="comment"
+              value={JSON.stringify(preview, null, 2)}
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              defaultValue={''}
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        linkedinData: {JSON.stringify(linkedinData)}
+        <br />
+        companyData: {companyData}
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,34 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">11x outreach tool</span>
+          </h2>
+          <div className="mt-8 flex justify-center">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="/new"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Send new outreach
+              </a>
+            </div>
+            <div className="ml-3 inline-flex">
+              <a
+                href="/history"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+              >
+                See history
+              </a>
+            </div>
+            
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
