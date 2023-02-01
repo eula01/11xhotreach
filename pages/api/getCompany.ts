790e5b3c6a3ba281709ee707d617a14827c3990a
eulambp <eula001@proton.me>
2023-02-01 19:28:31 +0000

    memoize gpt-3 api calls

--- pages/api/getCompany.ts
+++ pages/api/getCompany.ts
@@ -1,28 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import puppeteer from 'puppeteer';
 import { Configuration, OpenAIApi } from 'openai';
+import { PrismaClient } from '@prisma/client';
+import { url } from 'inspector';
 
-type ResData = {
-  [key: string]: string;
-};
+// define api clients outside of handler to avoid creating new clients for each request
+const prisma = new PrismaClient();
 
-// defined outside of function to reuse api client
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_SECRET,
 });
 const openai = new OpenAIApi(configuration);
-console.log(process.env.OPENAI_API_SECRET);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // check if we already have a summary to avoid expensive gpt-3 calls
+  const summary = await prisma.companysummaries.findMany({
+    where: {
+      site: req.body,
+    },
+  });
+
+  if (summary.length > 0) {
+    console.log('found summary in db');
+    // just return the first result for now, this can get tricky otherwise– i need to handle url variations
+    res.status(200).json({ body: summary[0].summary });
+  }
+
   // init puppeteer
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1080, height: 1024 });
-
-  console.log(req.body);
 
   // no need to handle `req.body.url === undefined`, input validation is client-side
   await page.goto(req.body);
@@ -38,6 +48,7 @@ export default async function handler(
     return window?.getSelection()?.toString();
   });
 
+  // ask gpt-3 to summarize all text from company website
   const completion = await openai.createCompletion({
     model: 'text-curie-001',
     prompt: `${text} \n\n Given the above text about a compnay, summarize what this company does`,
@@ -47,6 +58,13 @@ export default async function handler(
   console.log(completion.data.choices[0].text);
 
   await browser.close();
+
+  await prisma.companysummaries.create({
+    data: {
+      site: req.body,
+      summary: completion.data.choices[0].text || 'No summary found',
+    },
+  });
 
   res.status(200).json({ body: completion.data.choices[0].text });
 }
